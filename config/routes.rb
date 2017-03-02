59e5cedfd5f4ca6fb0694ebbe9334b06cfef3859
Carlos <carloslugo_ing@utp.edu.co>
2017-03-02 01:55:57 -0500

    motivado

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  
+
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
+
+  resources :articles
+#  get "/articles" index
+#  post "/articles"	create
+#  delete "/articles" delete
+#  get "/articles/:id" show
+#  get "articles/new" new
+#  get "/articles/:id/edit" edit
+#  patch "/articles/:id" update
+#  put "/articles/:id" update
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170302054321_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.string :location
+      t.integer :visits_count
+
+      t.timestamps
+    end
+  end
+end
