956e244cc892a3389dd864e2638cdd852973b835
Carlos <carloslugo_ing@utp.edu.co>
2017-03-08 23:01:42 -0500

    Final Mailer

--- app/mailers/action_correo_mailer.rb
+++ app/mailers/action_correo_mailer.rb
@@ -3,8 +3,8 @@ class ActionCorreoMailer < ApplicationMailer
 
 	def welcome_email(user)
 		@user = user
-		@url = 'http://localhost:3000'
-		mail(to: @user.email, subject: 'welcome to my awesome Page')
+		@url = 'https://young-shore-56554.herokuapp.com'
+		mail(to: @user.email, subject: 'Tu Publicacion Fue Exitosa')
 	end
 	
 end
