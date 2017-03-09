a2325aaf727e817ddab4352eeca81c27ef2f1d36
Carlos <carloslugo_ing@utp.edu.co>
2017-03-08 22:21:18 -0500

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => 'proyectoejetour@gmail.com',
-  :password             => 'auntenticacion',
+  :password             => 'autenticacion',
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
