0fecdf9ca279b40bc4da9cbf3233aac0ac29ffe8
Carlos <carloslugo_ing@utp.edu.co>
2017-04-15 17:23:51 -0500

    Personalizacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,31 @@
 Rails.application.routes.draw do
 
-  mount ActionCable.server, at: '/cable'
+  
 
-  resources :chatrooms do
-  	resource :chatroom_users
-  	resources :messages
+	resources :chatrooms do
+  		resource :chatroom_users
+  		resources :messages
 
-  end
+  	end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: 'chatrooms#index'
-
-
-
-  resources :articles
+	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+	root to: 'pages#home'
 
 
-#  get "/articles" index
-#  post "/articles"	create
-#  delete "/articles/:id" delete
-#  get "/articles/:id" show
-#  get "articles/new" new
-#  get "/articles/:id/edit" edit
-#  patch "/articles/:id" update
-#  put "/articles/:id" update
+
+	resources :articles
 
 
+	#  get "/articles" index
+	#  post "/articles"	create
+	#  delete "/articles/:id" delete
+	#  get "/articles/:id" show
+	#  get "articles/new" new
+	#  get "/articles/:id/edit" edit
+	#  patch "/articles/:id" update
+	#  put "/articles/:id" update
+	
+  	# Serve websocket cable requests in-process
+	mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
