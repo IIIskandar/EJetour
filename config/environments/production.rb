eea7f990367b2518b5b33079681bac1f5a2e61f3
Carlos <carloslugo_ing@utp.edu.co>
2017-04-15 00:30:21 -0500

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.mount_path = nil
+   config.action_cable.url = 'wss://young-shore-56554.herokuapp.com/cable'
+   config.action_cable.allowed_request_origins = [ 'https://young-shore-56554.herokuapp.com', /http:\/\/young-shore-56554.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
