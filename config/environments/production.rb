6dcb725b0dc541f6c0d99573ac202ac8dc668d59
Carlos <carloslugo_ing@utp.edu.co>
2017-04-15 01:44:35 -0500

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
    config.action_cable.mount_path = nil
    config.action_cable.url = 'wss://young-shore-56554.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'https://young-shore-56554.herokuapp.com', /http:\/\/young-shore-56554.herokuapp.*/ ]
+   config.action_cable.allowed_request_origins = [ 'https://young-shore-56554.herokuapp.com', /http:\/\/young-shore-56554.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  
-  resources :cable do
-  	resources :chatrooms 
+  resources :chatrooms do
   	resource :chatroom_users
   	resources :messages
-
+  	
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
